709561f6ba53c1086feb383e2ac8b067952a5b47
rubayamim381 <mimjkkniu.381@gmail.com>
2023-06-07 20:49:32 +0600

    copy constructor

--- Second_OOPS/src/Hi.java
+++
@@ -1,7 +0,0 @@
-public class Hi{
-	public static void main(String args[])
-	{
-		System.out.print("new code will be arrived soon");
-	}
-	
-}
---
+++ Second_OOPS/src/OOPS.java
@@ -0,0 +1,31 @@
+//another class
+class Student {
+	String name, id;
+	int age;
+	
+	public void info() {
+		System.out.println(this.name+" "+this.age);
+	}
+	
+	Student(Student s1) { //copy constructor
+		this.name = s1.name;
+		this.age = s1.age;
+	}
+	
+	Student(String name, int age){
+		this.name = name;
+		this.age = age;
+	}
+}
+
+
+public class OOPS{
+	public static void main(String args[])
+	{	
+		Student s1 = new Student("akash",25);
+		Student s2 = new Student(s1); //assign s1 properties to s2
+		s1.info();
+		s2.info();
+	}
+	
+}
