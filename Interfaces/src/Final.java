db25ad18afa0a08deb57c6653dad8cd677cb8484
rubayamim381 <mimjkkniu.381@gmail.com>
2023-07-01 19:08:13 +0600

    non-static final example

--- Interfaces/src/Final.java
+++ Interfaces/src/Final.java
@@ -1,13 +1,40 @@
 class B {
+	//static final variable and method
 	static final int n;
 	static {
 		n = 5;
 	}
+
+	static final void run() {
+		System.out.println("Satic final test and final var is: "+B.n);
+	}
+	
+	//non-static final variable and method
+	final int m;
+
+	B(){
+		m=15;
+	}
+	final void display() {
+		System.out.println("non-static final test and final var is: "+m);
+	}
 }
 
-public class Final {
+class C extends B{
+	
+}
+
+public class Final{
 	public static void main(String[] args) {
-		// called in static way
+		// if we use static, the variable and method should be called in static way
 		System.out.println(B.n);
+		B.run();
+		
+		//if we called final, they should be called in creating object
+		C c = new C();
+		c.display();
+		
+		B b = new B();
+		b.display();
 	}
 }
